233258f8f7f5b57573f9d4a8947fd0731494d96c
Chris Wundram <wundram@gmail.com>
2026-02-23 11:31:52 -0800

    Update sha256 for v0.1.0

--- Casks/markedit-in-context.rb
+++ Casks/markedit-in-context.rb
@@ -1,6 +1,6 @@
 cask "markedit-in-context" do
   version "0.1.0"
-  sha256 "3e310ced65613776278b4f0537f0b0a2319153cca4d03e4ae7e300d3a2ecbc1b"
+  sha256 "7ccbd2e97c8592e15519126aaa0bea43cb41c4139025654d618eeb368c8e5f55"
 
   url "https://github.com/wundram/MarkEdit-InContext/releases/download/v#{version}/MarkEdit-InContext-#{version}.zip"
   name "MarkEdit InContext"
